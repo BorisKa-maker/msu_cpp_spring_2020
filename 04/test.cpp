8fe55ec1f279ec573d85efc302202761d41580bf
BorisKa-maker <61497615+BorisKa-maker@users.noreply.github.com>
2020-04-28 08:48:08 +0300

    Add files via upload

---
+++ 04/test.cpp
@@ -0,0 +1,43 @@
+#include "Serializer.h"
+#include <cassert>
+#include <sstream>
+
+struct Data
+{
+    uint64_t a;
+    bool b;
+    uint64_t c;
+
+    template <class T>
+    Error serialize(T& serializer)
+    {
+        return serializer(a, b, c);
+    }
+
+};
+void test1() {
+
+
+    Data x{ 1, true, 2 };
+
+    std::stringstream stream;
+
+    Serializer serializer(stream);
+    serializer.save(x);
+
+    Data y{ 0, false, 0 };
+
+    Deserializer deserializer(stream);
+    const Error err = deserializer.load(y);
+
+    assert(err == Error::NoError);
+
+    assert(x.a == y.a);
+    assert(x.b == y.b);
+    assert(x.c == y.c);
+    //std::cout << y.b << std::endl;
+}
+int main() {
+    test1();
+    return 0;
+}
